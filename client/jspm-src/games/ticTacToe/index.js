3939bb5ffd770848d92f84a052bd7cd45152eff9
Amit Portnoy <amit.portnoy@gmail.com>
2016-03-09 08:03:11 +0200

    make tictactoe winner line into a template

--- client/jspm-src/games/ticTacToe/index.js
+++ client/jspm-src/games/ticTacToe/index.js
@@ -70,19 +70,6 @@ function end({statistics, game, outcome}) {
 }
 
 export function deserialize(room) {
-  if (room.game.isEnded && room.game.outcome !== 'tie') {
-    room.game.outcome.line =
-      {
-        'c0': {c1: 0, r1: 0, c2: 0, r2: 2},
-        'c1': {c1: 1, r1: 0, c2: 1, r2: 2},
-        'c2': {c1: 2, r1: 0, c2: 2, r2: 2},
-        'r0': {c1: 0, r1: 0, c2: 2, r2: 0},
-        'r1': {c1: 0, r1: 1, c2: 2, r2: 1},
-        'r2': {c1: 0, r1: 2, c2: 2, r2: 2},
-        'd0': {c1: 0, r1: 0, c2: 2, r2: 2},
-        'd1': {c1: 0, r1: 2, c2: 2, r2: 0}
-      }[room.game.outcome.line];
-  }
   room.game.markCell = (payload) => {
     room.io.emit('room:gameAction', {roomId: room.roomId, type: 'MARK_CELL', payload});
   };
--- client/jspm-src/modules/games/ticTacToe/index.js
+++ client/jspm-src/modules/games/ticTacToe/index.js
@@ -1,4 +1,7 @@
-import template from './board.html!text';
+import boardTpl from './board.html!text';
+import winnerLineTpl from './winner-line.html!text';
+import cellTpl from './cell.html!text';
+
 import './index.css!';
 
 const clientPath = 'modules/games/ticTacToe/';
@@ -6,13 +9,49 @@ const clientPath = 'modules/games/ticTacToe/';
 import module from '../base';
 
 module.run(['$templateCache', function ($templateCache) {
-  $templateCache.put('TicTacToe', template);
+  $templateCache.put('TicTacToe', '<tic-tac-toe game="$ctrl.game" players="$ctrl.players" own="$ctrl.own"></tic-tac-toe>');
 }]);
+
+module.component('ticTacToe', {
+  template: boardTpl,
+  bindings: {
+    game: '<',
+    players: '<',
+    own: '<'
+  },
+  controller: class {
+    $onInit() {
+    }
+  }
+});
+
+module.component('winnerLine', {
+  template: winnerLineTpl,
+  bindings: {
+    line: '@',
+    color: '@'
+  },
+  controller: class WinnerLineController {
+    static line2points = {
+      'c0': {c1: 0, r1: 0, c2: 0, r2: 2},
+      'c1': {c1: 1, r1: 0, c2: 1, r2: 2},
+      'c2': {c1: 2, r1: 0, c2: 2, r2: 2},
+      'r0': {c1: 0, r1: 0, c2: 2, r2: 0},
+      'r1': {c1: 0, r1: 1, c2: 2, r2: 1},
+      'r2': {c1: 0, r1: 2, c2: 2, r2: 2},
+      'd0': {c1: 0, r1: 0, c2: 2, r2: 2},
+      'd1': {c1: 0, r1: 2, c2: 2, r2: 0}
+    };
+    $onInit() {
+      this.points = WinnerLineController.line2points[this.line];
+    }
+  }
+});
 
 module.directive('cell', function () {
   return {
     scope: true,
-    templateUrl: `${clientPath}cell.html`,
+    template: cellTpl,
     link: function (scope, iElement, iAttrs) {
 
       const x = scope.x = iAttrs.x;
